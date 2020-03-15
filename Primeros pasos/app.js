3922039001ee3f9de443bb1283886c9477d4cb9f
Jose Gonzalez <jgonzalezr5@outlook.es>
2020-03-15 20:26:09 -0300

    Añadiendo nodemon

--- Primeros pasos/app.js
+++ Primeros pasos/app.js
@@ -2,7 +2,7 @@ const sum = require('./sum');
 
 const http  = require('http');
 const server = http.createServer((req, res) => {
-    res.end('Hello World from Server js');
+    res.end('Hello World from js');
 })
 
 server.listen(3000);
