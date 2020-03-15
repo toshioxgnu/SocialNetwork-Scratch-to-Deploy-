5bd46fb0f7c97d630443cdc03df5092c76d4cf4a
Jose Gonzalez <jgonzalezr5@outlook.es>
2020-03-15 20:20:09 -0300

    Añadiendo npm

---
+++ Primeros pasos/app.js
@@ -0,0 +1,10 @@
+const sum = require('./sum');
+
+const http  = require('http');
+const server = http.createServer((req, res) => {
+    res.end('Hello World from Server js');
+})
+
+server.listen(3000);
+const total = sum.sum(410,250);
+console.log("Total: ",total);
--- Primeros pasos/node.js
+++
@@ -1,3 +0,0 @@
-const sum = require('./sum')
-const total = sum.sum(410,250);
-console.log("Total: ",total);
